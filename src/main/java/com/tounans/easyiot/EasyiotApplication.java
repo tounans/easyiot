6f6b8cb557930656832bf16f9fa4edb1e8d7dbe5
tounans <tounans@outlook.com>
2019-12-28 16:41:22 +0800

    111

---
+++ src/main/java/com/tounans/easyiot/EasyiotApplication.java
@@ -0,0 +1,13 @@
+package com.tounans.easyiot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyiotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyiotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tounans/easyiot/EasyiotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tounans.easyiot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyiotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
